d08a2b78be8e523e049a39ce7c9f7739116612c9
girassollari <134282899+girassollari@users.noreply.github.com>
2024-03-18 08:58:19 -0300

    Update main.js

--- main.js
+++ main.js
@@ -43,14 +43,15 @@ function calculaTempo(tempoObjetivo) {
  return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
   }
   
-  function atualizaCronometro()
-  document.getElementById("dias0").textContent = calculaTempo(tempos[1])[0]
-  document.getElementById("horas0").textContent = calculaTempo(tempos[1])[1];
-  document.getElementById("min0").textContent = calculaTempo(tempos[1])[2];
-  document.getElementById("seg0").textContent = calculaTempo(tempos[1])[3];;
-for (let i = 0; i < contadores.length; i++) {
-   // contadores[i].textContent = calculaTempo(tempos[i]); 
-}
+  function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
+        console.log("seg"+i);
+    }
+  }
 
 function comecaCronometro(){
     atualizaCronometro();
